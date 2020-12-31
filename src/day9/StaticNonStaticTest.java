e1be005b39c7e2b38ef21ecfd28071fa96614bce
Ashish Shrestha <ashish2018shrestha@gmail.com>
2020-12-30 23:25:37 -0500

    changes on line 8 in StsticNonStaticTest

--- src/day9/StaticNonStaticTest.java
+++ src/day9/StaticNonStaticTest.java
@@ -5,7 +5,7 @@ public class StaticNonStaticTest {
     public static void main(String[] args) {
         StaticNonStaticDemo ob = new StaticNonStaticDemo();
         ob.nonStaticMethod();
-        System.out.println(ob.nonStaticMethod());
+        System.out.println(ob.nonStaticVar);
 
         StaticNonStaticDemo.staticMethod();
         System.out.println(StaticNonStaticDemo.staticVar);
